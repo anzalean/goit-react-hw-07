4807df57e0028474468fb9526a03f31d5ba115fd
anzalean <zablovskaya04@gmail.com>
2024-10-27 14:21:58 +0200

    fixed mistakes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,18 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // Використовуємо localStorage
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
-
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'], 
-};
-
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer,
 });
 
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-
-export const store = configureStore({
-  reducer: persistedReducer,
+const store = configureStore({
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER], 
-      },
+      serializableCheck: false,
     }),
 });
 
-export const persistor = persistStore(store);
+export default store;
